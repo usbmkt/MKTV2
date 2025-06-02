563115975daff8b852e8561a1891f435ade4d0a8
usbmkt <usbmktpro@gmail.com>
2025-06-02 20:09:00 -0300

    Update drizzle.config.ts

--- zap/drizzle.config.ts
+++ zap/drizzle.config.ts
@@ -8,7 +8,7 @@ if (!DATABASE_URL) {
   console.warn(' ATENÇÃO: Variável ZAP_DATABASE_URL não definida no arquivo .env.zap');
   console.warn(' Drizzle Kit pode falhar ou usar um valor padrão inesperado.');
   console.warn(' Crie um arquivo .env.zap na raiz da pasta "zap/" com:');
-  console.warn(' ZAP_DATABASE_URL=sua_url_postgres');
+  console.warn(' ZAP_DATABASE_URL=postgresql://mktv5renderuser:3CWm0J0MNNQAXdh71LBTDehOQLERdxig@dpg-d0t7v1u3jp1c73eaajpg-a.oregon-postgres.render.com/mktv5render_wb67');
   console.warn('------------------------------------------------------------------');
   // NÃO encerra o processo, pois comandos como 'generate' podem não exigir conexão
 }
