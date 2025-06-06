456a8bef702271822299e02193c9b02fb76529f3
usbmkt <usbmktpro@gmail.com>
2025-06-06 09:32:21 -0300

    Create logger.ts

---
+++ server/logger.ts
@@ -0,0 +1,20 @@
+// server/logger.ts
+import pino from 'pino';
+
+// Configura um logger mais legível para o ambiente de desenvolvimento
+const transport = pino.transport({
+  target: 'pino-pretty',
+  options: {
+    colorize: true,
+    translateTime: 'SYS:HH:MM:ss',
+    ignore: 'pid,hostname',
+  },
+});
+
+// Exporta a instância do logger para ser usada em toda a aplicação de backend
+export const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+  process.env.NODE_ENV !== 'production' ? transport : undefined,
+);
